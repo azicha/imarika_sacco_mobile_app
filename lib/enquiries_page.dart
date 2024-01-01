22692176ac30ac5147647f0f45f825449dbd4c9c
azicha <askakauchi@gmail.com>
2024-01-01 14:31:46 +0300

    Add New Page for Enquiries

---
+++ lib/enquiries_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EnquiriesPage extends StatelessWidget {
+  const EnquiriesPage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+   return  Scaffold(
+    appBar: AppBar(
+      backgroundColor: const Color.fromARGB(255, 231, 230, 233),
+      title: const Text('Back'),),
+    body: const Column(
+      children: [],
+    ),
+   );
+  }
+}
