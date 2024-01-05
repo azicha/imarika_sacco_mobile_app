c5496a7386968ac93519a2c2c4dcd074726bd2ef
azicha <askakauchi@gmail.com>
2024-01-05 22:40:31 +0300

    Add new page for Deposit

---
+++ lib/deposit_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DepositPage extends StatelessWidget {
+  const DepositPage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
