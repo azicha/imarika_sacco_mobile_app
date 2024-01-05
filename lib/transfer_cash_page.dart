f07f2ce5bacc943578885665d45fe9e822ffc17b
azicha <askakauchi@gmail.com>
2024-01-05 22:43:34 +0300

    Add new page for tranferrals

---
+++ lib/transfer_cash_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TransferCashPage extends StatelessWidget {
+  const TransferCashPage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
