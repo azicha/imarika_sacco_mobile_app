a33ccfa52f0269fd7b01a42481c6c8cc56380a4a
azicha <askakauchi@gmail.com>
2024-01-10 23:23:32 +0300

    Add new page for a full statement page

---
+++ lib/full_statement_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FullStatementPage extends StatelessWidget {
+  const FullStatementPage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
