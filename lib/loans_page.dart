878ed5d56e867af031acecaad05af1da21e4bc30
azicha <askakauchi@gmail.com>
2024-03-12 17:29:19 +0300

    Code refactoring

--- lib/loans_page.dart
+++ lib/loans_page.dart
@@ -10,6 +10,93 @@ class LoansPage extends StatefulWidget {
 class _LoansPageState extends State<LoansPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        width: 1.0,
+        style: BorderStyle.none,
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Request Loan'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.local_atm_outlined,
+                        size: 46,
+                      ),
+                      Text(
+                        'Loan Limit',
+                        style: TextStyle(
+                            fontSize: 32, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        '0.00',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: 'SELECT DURATION',
+                hintStyle: TextStyle(color: Colors.black26),
+                focusedBorder: border,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: 'ENTER AMOUNT',
+                hintStyle: TextStyle(color: Colors.black26),
+                focusedBorder: border,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () {},
+                style: const ButtonStyle(
+                  foregroundColor: MaterialStatePropertyAll(Colors.white),
+                  backgroundColor: MaterialStatePropertyAll(Colors.deepPurple),
+                  fixedSize: MaterialStatePropertyAll(
+                    Size(150, 40),
+                  ),
+                ),
+                child: const Text('Submit'))
+          ],
+        ),
+      ),
+    );
   }
 }
